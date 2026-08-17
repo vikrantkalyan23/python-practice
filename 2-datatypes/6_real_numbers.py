8bf4ffb32265d29be591053eeb8518481b2b1d84
Vikrant Kalyan <vikrant.kalyan23@gmail.com>
2026-08-17 17:37:10 +0530

    feat : string and real numbers

---
+++ 2-datatypes/6_real_numbers.py
@@ -0,0 +1,7 @@
+import sys
+
+a = 10.5
+b = 10.49
+
+print(f"Difference : {a - b}")
+print(sys.float_info)
---
+++ 2-datatypes/7_string.py
@@ -0,0 +1,20 @@
+"""String is immutable"""
+
+name = "My name is Vikrant"
+
+# Indexing, Slicing, Encoding, Decoding
+
+print(f"Complete String : {name}")
+print(f"Complete String : {name[0:]}")
+print(f"Initial 7 Characters : {name[0:7]}")
+print(f"7 to 10  Characters  : {name[7:10]}")
+print(f"7 onwards  : {name[7:]}")
+print(f"Reverse  : {name[::-1]}")
+print(f"Skip 2 Characters  : {name[::2]}")
+
+address ="Las vegas 😍"
+
+encoded_string = address.encode("utf-8")
+print(f"Encoded String : {encoded_string}")
+coded_string = encoded_string.decode("utf-8")
+print(f"Decoded String : {coded_string}")
