999b1cf18e75562202201a3d4de15db77584e3ef
Vikrant Kalyan <vikrant.kalyan23@gmail.com>
2026-08-17 12:20:22 +0530

    feat : integer and boolean

---
+++ 2-datatypes/4_boolean.py
@@ -0,0 +1,13 @@
+
+is_active = 0 # 1 = True
+is_admin = True
+is_otp_verified = "No"
+is_mobile_verified = None
+is_email_verified = False
+
+print(f"is_active : {bool(is_active)}")
+print(f"is_admin : {bool(is_admin)}")
+print(f"is_otp_verified : {bool(is_otp_verified)}")
+print(f"is_mobile_verified : {bool(is_mobile_verified)}")
+print(f"is_email_verified : {bool(is_email_verified)}")
+
