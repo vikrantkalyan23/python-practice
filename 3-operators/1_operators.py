893d920ed3b55234d68ba49b4737b9e0ddf9e261
Vikrant Kalyan <vikrant.kalyan23@gmail.com>
2026-08-23 23:00:41 +0530

    feat : operators

---
+++ 3-operators/1_operators.py
@@ -0,0 +1,31 @@
+# An operator is a symbol or keyword that tells Python to perform an operation on one or more values.
+
+a = 10
+b = 5
+result = a + b
+print(result)
+
+# a + b
+# │ │ │
+# │ │ └── operand
+# │ └──── operator
+# └────── operand
+
+# Categories of Python Operators
+# Python Operators
+# │
+# ├── 1. Arithmetic Operators
+# │
+# ├── 2. Assignment Operators
+# │
+# ├── 3. Comparison Operators
+# │
+# ├── 4. Logical Operators
+# │
+# ├── 5. Identity Operators
+# │
+# ├── 6. Membership Operators
+# │
+# ├── 7. Bitwise Operators
+# │
+# └── 8. Conditional Expression
---
+++ 3-operators/2_arithmetic_operators.py
@@ -0,0 +1,43 @@
+# Arithmetic Operators (Arithmetic operators are used for mathematical calculations)
+
+a = 10
+b = 5
+
+# + Addition
+print("Addition : ", a + b)
+
+# - Subtraction
+print("Subtraction : ", a - b)
+
+# * Multiplication
+print("Multiplication : ", a * b)
+
+# / Division
+print("Division : ", a / b)
+
+# // Floor Division (Floor division returns the quotient rounded down)
+print("Floor Division : ", a / b)
+
+# % Modulus (Returns the remainder)
+print("Remainder : ", 10 % 3)
+# Very common use: even/odd
+number = 10
+if number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+# ** Exponentiation (Used for powers)
+print("Exponentiation : ", 2**3)
+
+# Arithmetic Operator Summary
+
+# | Operator | Meaning        |   Example |     Result |
+# | -------- | -------------- | --------: | ---------: |
+# | `+`      | Addition       |  `10 + 3` |       `13` |
+# | `-`      | Subtraction    |  `10 - 3` |        `7` |
+# | `*`      | Multiplication |  `10 * 3` |       `30` |
+# | `/`      | Division       |  `10 / 3` | `3.333...` |
+# | `//`     | Floor division | `10 // 3` |        `3` |
+# | `%`      | Remainder      |  `10 % 3` |        `1` |
+# | `**`     | Power          | `10 ** 3` |     `1000` |
