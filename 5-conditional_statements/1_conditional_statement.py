bc9afa421d8f2d204b021a3b893012770d50310e
Vikrant Kalyan <vikrant.kalyan23@gmail.com>
2026-08-24 20:25:34 +0530

    feat : conditional statement

---
+++ 5-conditional_statements/1_conditional_statement.py
@@ -0,0 +1,16 @@
+"""Conditional statements allow your program to make decisions.
+In simple words:
+If something is true, do this; otherwise, do something else."""
+
+age = 20
+if age >= 18:
+    print("You are an adult")
+
+# Without conditions, a program would always execute the same instructions.
+"""
+Login
+ ↓
+Is username/password correct?
+ ├── Yes → Login successful
+ └── No  → Login failed
+ """
